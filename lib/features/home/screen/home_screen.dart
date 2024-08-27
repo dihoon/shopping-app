88a23482ae10c414406580b2275c3bb703085c04
dihoon <dihoon955@gmail.com>
2024-08-27 09:13:53 +0900

    first commit

---
+++ lib/features/home/screen/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/common/layer/default_layout.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+        child: Column(
+      children: [],
+    ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/features/home/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
